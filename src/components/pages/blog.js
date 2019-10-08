69dfa1ac2e318436cfb97731e6fe67fe344b1a89
Jrodan88 <jayoso88@gmail.com>
2019-10-08 00:29:01 -0600

    Revisiting Render Props and Passing the Logged In Status to Child Components

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -101,11 +101,12 @@ class Blog extends Component {
         handleModalClose={this.handleModalClose} 
         modalIsOpen={this.state.blogModalIsOpen} />
 
+        {this.props.loggedInStatus === "LOGGED_IN" ?
         <div className="new-blog-link">
           <a onClick={this.handleNewBlogClick}>
             <FontAwesomeIcon icon="plus-square" />
           </a>
-        </div>    
+        </div>  : null }   
 
 
         <div className="content-container">{blogRecords}</div>
